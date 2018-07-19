8e5ad055fb7152b505010404d22947d19f0f5350
Alexey Shmelkov <ashmelkov@aligntech.com>
2018-07-19 13:31:13 +0300

    New chapter

---
+++ Part10/passengers.js
@@ -0,0 +1,5 @@
+var passengers =
+    [{ name: "Jane Doloop", paid: true },
+    { name: "Dr. Evel", paid: true },
+    { name: "Sue Property", paid: false },
+    { name: "John Funcall", paid: true }];
