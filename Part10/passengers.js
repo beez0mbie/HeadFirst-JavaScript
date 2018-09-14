ba8bf6aba08f756a4ac81af143210b47dd44dab0
z0mbie-bee <vzhik3268@gmail.com>
2018-09-14 11:20:12 +0300

    add two functions

--- Part10/passengers.js
+++ Part10/passengers.js
@@ -2,4 +2,20 @@ var passengers =
     [{ name: "Jane Doloop", paid: true },
     { name: "Dr. Evel", paid: true },
     { name: "Sue Property", paid: false },
-    { name: "John Funcall", paid: true }];
+    { name: "John Funcall", paid: true },
+    { name: "Maxim Klimovich", paid: false},
+    { name: "Alexander Tarasov", paid: false}];
+
+function chekNoflyList(passenger) {
+    if ("Doctor Evil" === passenger) {
+        return true
+    }
+    return false
+}
+
+function checkNoPaid(passenger) {
+    if (passenger === false){
+        return true
+    }
+    return true
+}
