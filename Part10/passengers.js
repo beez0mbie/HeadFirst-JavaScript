e6b3886cd0f05d8f0ddb350c683ef28d03b33ce2
Alexey Shmelkov <ashmelkov@aligntech.com>
2018-09-17 11:18:54 +0300

    [p.468] add function for function passengers

--- Part10/passengers.js
+++ Part10/passengers.js
@@ -7,15 +7,36 @@ var passengers =
     { name: "Alexander Tarasov", paid: false}];
 
 function chekNoflyList(passenger) {
-    if ("Doctor Evil" === passenger) {
-        return true
-    }
-    return false
+    return ("Dr. Evel" === passenger.name)
 }
 
 function checkNoPaid(passenger) {
-    if (passenger === false){
-        return true
+    return (!passenger.paid)
+}
+
+function processPassengers(passengers, testFunction) {
+    for (let i = 0; i < passengers.length; i++) {
+        if (testFunction(passengers[i])) {
+            return false
+        }  
     }
     return true
-} 
+}
+
+function printPassenger(passenger){
+    console.log("Name: "+ passenger.name + "\nPaid: " + passenger.paid)
+}
+
+var allCanFly = processPassengers(passengers, chekNoflyList)
+
+if (!allCanFly) {
+    console.log("The plane can't take off: we have passengers on the no-fly-list.")
+}
+
+var allPaid = processPassengers(passengers, checkNoPaid)
+
+if (!allPaid) {
+    console.log("The plane can't take off: not everyone has paid")
+}
+
+var printListOfPassengers = processPassengers(passengers, printPassenger)
