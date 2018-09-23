5d85167417cc59d2c6b3fce7d359a9d6dcb10bb7
Alexey Shmelkov <ashmelkov@aligntech.com>
2018-09-23 17:29:26 +0300

    new exercise p.469

--- Part10/passengers.js
+++ Part10/passengers.js
@@ -3,8 +3,8 @@ var passengers =
     { name: "Dr. Evel", paid: true },
     { name: "Sue Property", paid: false },
     { name: "John Funcall", paid: true },
-    { name: "Maxim Klimovich", paid: false},
-    { name: "Alexander Tarasov", paid: false}];
+    { name: "Maxim Klimovich", paid: false },
+    { name: "Alexander Tarasov", paid: false }];
 
 function chekNoflyList(passenger) {
     return ("Dr. Evel" === passenger.name)
@@ -18,13 +18,13 @@ function processPassengers(passengers, testFunction) {
     for (let i = 0; i < passengers.length; i++) {
         if (testFunction(passengers[i])) {
             return false
-        }  
+        }
     }
     return true
 }
 
-function printPassenger(passenger){
-    console.log("Name: "+ passenger.name + "\nPaid: " + passenger.paid)
+function printPassenger(passenger) {
+    console.log("Name: " + passenger.name + "\nPaid: " + passenger.paid)
 }
 
 var allCanFly = processPassengers(passengers, chekNoflyList)
