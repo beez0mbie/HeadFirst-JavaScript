ad05d90034aaae6f3b622e5dea5f783b9620bdc5
Alexey Shmelkov <ashmelkov@aligntech.com>
2018-09-26 10:52:36 +0300

    add new functions to passengers (drink and eat)

--- Part10/passengers.js
+++ Part10/passengers.js
@@ -1,10 +1,10 @@
 var passengers =
-    [{ name: "Jane Doloop", paid: true },
-    { name: "Dr. Evel", paid: true },
-    { name: "Sue Property", paid: false },
-    { name: "John Funcall", paid: true },
-    { name: "Maxim Klimovich", paid: false },
-    { name: "Alexander Tarasov", paid: false }];
+    [{ name: "Jane Doloop", paid: true, ticket: "coach" },
+    { name: "Dr. Evel", paid: true, ticket: "firstclass" },
+    { name: "Sue Property", paid: false, ticket: "firstclass" },
+    { name: "John Funcall", paid: true, ticket: "coach" },
+    { name: "Maxim Klimovich", paid: false, ticket: "bestCoach" },
+    { name: "Alexander Tarasov", paid: false, ticket: "coach" }];
 
 function chekNoflyList(passenger) {
     return ("Dr. Evel" === passenger.name)
@@ -40,3 +40,51 @@ if (!allPaid) {
 }
 
 var printListOfPassengers = processPassengers(passengers, printPassenger)
+
+function serveCustomer(passenger){
+    var getDrinkOrderFunction = createDrinkOrder(passenger)
+    var getEatOrderFunction = createEatOrder(passenger)
+    getDrinkOrderFunction()
+    getEatOrderFunction()
+}
+
+function createDrinkOrder(passenger){
+    var orderFunction;
+
+    if (passenger.ticket === "firstclass"){
+        orderFunction = function(){
+            alert(passenger.name + " Would you like a cocktail or wine?");
+        }
+    } else if (passenger.ticket ==="bestCoach") {
+        orderFunction = function(){
+            alert(passenger.name + " Would you like water, cola or wine?");
+        }
+    } else {
+        orderFunction = function(){
+            alert(passenger.name + " Your choice is cola or water.");
+        }
+    }
+    return orderFunction;
+}
+
+function createEatOrder(passenger){
+    var orderFunction;
+
+    if (passenger.ticket === "firstclass"){
+        orderFunction = function(){
+            alert(passenger.name + " Would you like a chicken or pasta?");
+        }
+    } else if (passenger.ticket ==="bestCoach") {
+        orderFunction = function(){
+            alert(passenger.name + " Would you like a snack box or cheese plate?");
+        }
+    } else {
+        orderFunction = function(){
+            alert(passenger.name + " Your choice is pretzels or peanuts.");
+        }
+    }
+    return orderFunction;
+}
+
+
+var servePassengers =  processPassengers(passengers, serveCustomer)
