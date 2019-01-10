d1681947f4ff58f61534318e7d58d28e9ec66312
Alexey Shmelkov <ashmelkov@aligntech.com>
2019-01-10 14:39:42 +0300

    add 549 exersize

---
+++ Part12/p549exersize.js
@@ -0,0 +1,26 @@
+function Coffee(roast, ounces) {
+    this.roast = roast;
+    this.ounces = ounces;
+
+    this.getSize = function(){
+        if(this.ounces <= 8){
+            return('small')
+        } else if(8 < this.ounces && this.ounces <= 12){
+            return('medium')
+        } else {
+            return('large')
+        }
+    }
+    this.toString = function(){
+       return('You`ve ordered a ' + this.getSize() + ' ' + this.roast + ' coffee');
+    }
+}
+
+var houseBlend = new Coffee('House Blend', 12);
+console.log(houseBlend.toString());
+
+var darkRoast = new Coffee('Dark Roast', 16);
+console.log(darkRoast.toString());
+
+var espresso = new Coffee('Espresso', 8);
+console.log(espresso.toString());
